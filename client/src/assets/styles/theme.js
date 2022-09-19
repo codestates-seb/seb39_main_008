f8d66828432870b36b05bbf127f2d35d3ec458b6
Swan <adeliine.0618@gmail.com>
2022-09-20 01:06:40 +0900

    Feat : Add theme properties

--- client/src/assets/styles/theme.js
+++ client/src/assets/styles/theme.js
@@ -1,26 +1,53 @@
-export const theme = {
-  colors: {
-    white: '#ffffff',
-    black: '#0c0d0e',
-    yellow: '#fbf3d5',
-    green: '#5eba7d',
-    blue: '#0074cc',
-    powder: '#e1ecf4',
-    red: '#d0393e',
-    black025: '#f7fafa',
-    black050: '#f1f2f3',
-    black075: '#e3e6e8',
-    black100: '#d6d9dc',
-    black150: '#c8ccd0',
-    black200: '#babfc4',
-    black300: '#9fa6ad',
-    black350: '#9199a1',
-    black400: '#838c95',
-    black500: '#6a737c',
-    black600: '#525960',
-    black700: '#3b4045',
-    black750: '#2f3337',
-    black800: '#232629',
-    black900: '#0c0d0e',
-  },
+const colors = {
+  white: '#ffffff',
+  black: '#0c0d0e',
+  grey: '#c8ccd0',
+  yellow: '#fbf3d5',
+  green: '#5eba7d',
+  blue: '#0074cc',
+  powder: '#e1ecf4',
+  red: '#d0393e',
+  black025: '#f7fafa',
+  black050: '#f1f2f3',
+  black075: '#e3e6e8',
+  black100: '#d6d9dc',
+  black150: '#c8ccd0',
+  text5: '#babfc4',
+  text4: '#6a737c',
+  text3: '#3b4045',
+  text2: '#232629',
+  text1: '#0c0d0e',
 };
+const fontSize = {
+  fontSizeLL: '36px',
+  fontSizeL: '24px',
+  fontSizeM: '16px',
+  fontSizeS: '12px',
+};
+
+const borderRadius = {
+  borderRadiusL: '12px',
+  borderRadiusM: '8px',
+  borderRadiusS: '4px',
+};
+const space = {
+  spaceL: '40px',
+  spaceM: '20px',
+  spaceS: '10px',
+};
+
+const layout = {
+  flexCenter: `
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`,
+  flexCenterColumn: `
+display: flex;
+flex-direction: column;
+justify-contents: center;
+align-items: center;
+`,
+};
+
+export const theme = { colors, fontSize, borderRadius, space, layout };
