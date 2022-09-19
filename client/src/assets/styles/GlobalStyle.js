2c5e739d85849354286bc1d16cb961ba82ad7542
0SCAR0421 <oscar0421@kakao.com>
2022-09-19 20:40:01 +0900

    Style : modify font cdn

--- client/src/assets/styles/GlobalStyle.js
+++ client/src/assets/styles/GlobalStyle.js
@@ -3,7 +3,6 @@ import { reset } from 'styled-reset';
 
 export const GlobalStyle = createGlobalStyle`
 ${reset};
-@import url('https://fonts.googleapis.com/css2?family=Fira+Sans+Extra+Condensed:ital,wght@1,100&family=Gowun+Batang:wght@400;700&display=swap');
 * {
   box-sizing: border-box;
 }
