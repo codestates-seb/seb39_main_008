74dae9c0f6cfaf3b50d8b9c179966ac93301e094
Swan <adeliine.0618@gmail.com>
2022-09-21 01:22:42 +0900

    Feat : Add responsive properties

--- client/src/assets/styles/theme.js
+++ client/src/assets/styles/theme.js
@@ -26,6 +26,15 @@ const fontSize = {
   fontSizeS: '12px',
 };
 
+const boxShadow = {
+  shadowXS: '0px 1px 4px -1px rgba(28, 48, 74, 0.5)',
+  shadowS: 'rgba(0, 0, 0, 0.05) 0px 1px 2px 0px',
+  shadowM:
+    '0px 8px 24px -4px rgba(28, 50, 79, 0.38), 0px 2px 6px 0px rgba(28, 55, 90, 0.16)',
+  shadowL:
+    '0px 12px 48px -6px rgba(28, 50, 79, 0.38), 0px 3px 18px -2px rgba(28, 55, 90, 0.16)',
+  shadowXL: 'rgba(33, 35, 38, 0.1) 0px 10px 10px -10px',
+};
 const borderRadius = {
   borderRadiusL: '12px',
   borderRadiusM: '8px',
@@ -35,6 +44,11 @@ const space = {
   spaceL: '40px',
   spaceM: '20px',
   spaceS: '10px',
+};
+const screen = {
+  mobile: `(max-width: 576px)`,
+  tablet: `(min-width: 768px) and (max-width: 991.98px)`,
+  desctop: `(min-width: 992)`,
 };
 
 const layout = {
@@ -51,4 +65,12 @@ align-items: center;
 `,
 };
 
-export const theme = { colors, fontSize, borderRadius, space, layout };
+export const theme = {
+  colors,
+  fontSize,
+  borderRadius,
+  space,
+  layout,
+  screen,
+  boxShadow,
+};
