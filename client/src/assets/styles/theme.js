5701da0a09a5f89633ad39e30b734217c97da04b
Swan <adeliine.0618@gmail.com>
2022-09-21 17:42:52 +0900

    Feat : 모듈 내보내기 변경

--- client/src/assets/styles/theme.js
+++ client/src/assets/styles/theme.js
@@ -1,4 +1,4 @@
-const colors = {
+export const colors = {
   dimGrey: '#babfc47d',
   white: '#ffffff',
   black: '#0c0d0e',
@@ -19,14 +19,14 @@ const colors = {
   text2: '#232629',
   text1: '#0c0d0e',
 };
-const fontSize = {
+export const fontSize = {
   fontSizeLL: '36px',
   fontSizeL: '24px',
   fontSizeM: '16px',
   fontSizeS: '12px',
 };
 
-const boxShadow = {
+export const boxShadow = {
   shadowXS: 'rgba(0, 0, 0, 0.3) 0px 3px 6px, rgba(0, 0, 0, 0.22) 0px 3px 4px',
   shadowS: 'rgba(0, 0, 0, 0.3) 0px 7px 14px, rgba(0, 0, 0, 0.22) 0px 6px 6px',
   shadowM: 'rgba(0, 0, 0, 0.3) 0px 11px 22px, rgba(0, 0, 0, 0.22) 0px 9px 8px',
@@ -35,23 +35,23 @@ const boxShadow = {
   shadowXL:
     'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px',
 };
-const borderRadius = {
+export const borderRadius = {
   borderRadiusL: '12px',
   borderRadiusM: '8px',
   borderRadiusS: '4px',
 };
-const space = {
+export const space = {
   spaceL: '40px',
   spaceM: '20px',
   spaceS: '10px',
 };
-const screen = {
+export const screen = {
   mobile: `(max-width: 576px)`,
   tablet: `(min-width: 576px) and (max-width: 991.98px)`,
   desktop: `(min-width: 992px)`,
 };
 
-const layout = {
+export const layout = {
   flexCenter: `
   display: flex;
   align-items: center;
