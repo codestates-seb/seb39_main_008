f7a78dd89302b27090aac0f8166598468ac20cba
0SCAR0421 <oscar0421@kakao.com>
2022-09-21 17:50:49 +0900

    Feat : profile, usercard 컴포넌트 작성

--- client/src/components/common/UserCard.js
+++ client/src/components/common/UserCard.js
@@ -1,5 +1,110 @@
-const UserCard = () => {
-  return <div>UserCard</div>;
+import styled from 'styled-components';
+import { theme } from '../../assets/styles/theme';
+import { FiUserCheck, FiUserX, FiBook } from 'react-icons/fi';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
+
+const UserCardContainer = styled.div`
+  width: 150px;
+  height: 175px;
+  padding: ${theme.space.spaceS};
+  border-radius: ${theme.borderRadius.borderRadiusS};
+  box-shadow: ${theme.boxShadow.shadowS};
+`;
+
+const UserCardContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: ${theme.fontSize.fontSizeS};
+  padding: ${theme.space.spaceS} 0;
+
+  > p {
+    color: ${theme.colors.text4};
+  }
+
+  > p:hover {
+    color: ${theme.colors.text2};
+  }
+
+  > div {
+    display: flex;
+  }
+
+  > div > p {
+    margin-left: ${theme.space.spaceS};
+  }
+
+  > div > div > svg {
+    cursor: pointer;
+  }
+`;
+
+const UserCard = ({
+  nickname,
+  userImage,
+  total_content,
+  isFollow,
+  follower,
+  memberId,
+}) => {
+  const [isCurrnetFollow, setIsFollow] = useState(null);
+  const [currnetFollower, setCurrnetFollower] = useState(null);
+
+  useEffect(() => {
+    setIsFollow(isFollow);
+    setCurrnetFollower(follower);
+  }, [isFollow]);
+
+  const handleFollow = async () => {
+    try {
+      // 팔로우 요청 함수 자리
+      if (isCurrnetFollow) {
+        setCurrnetFollower((prev) => --prev);
+      } else {
+        setCurrnetFollower((prev) => ++prev);
+      }
+      setIsFollow(!isCurrnetFollow);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <UserCardContainer>
+      <Avatar
+        alt={nickname}
+        imageURL={userImage}
+        width={'100%'}
+        height={'131px'}
+        borderRadius={theme.borderRadius.borderRadiusS}
+      />
+      <UserCardContent>
+        <p>
+          <Link to={`/user/${memberId}`}>{nickname}</Link>
+        </p>
+        <div>
+          <div>
+            {isCurrnetFollow ? (
+              <>
+                {currnetFollower} <FiUserCheck onClick={handleFollow} />
+              </>
+            ) : (
+              <>
+                {currnetFollower} <FiUserX onClick={handleFollow} />
+              </>
+            )}
+          </div>
+          <p>
+            {total_content}{' '}
+            <Link to={`/books/${memberId}`}>
+              <FiBook />
+            </Link>
+          </p>
+        </div>
+      </UserCardContent>
+    </UserCardContainer>
+  );
 };
 
 export default UserCard;
