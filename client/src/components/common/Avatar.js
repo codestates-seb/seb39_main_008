b3a905b41a10f0ba6d8316d8ccd44eed21e46759
Swan <adeliine.0618@gmail.com>
2022-09-22 01:10:58 +0900

    Feat : 쉐도우 props 설정 추가

--- client/src/components/common/Avatar.js
+++ client/src/components/common/Avatar.js
@@ -1,14 +1,17 @@
 import styled from 'styled-components';
-
+import { boxShadow } from '../../assets/styles/theme';
 const ProfileURL = styled.img`
   width: ${(props) => props.width || '100%'};
   height: ${(props) => props.height || '100%'};
   border-radius: ${(props) => props.borderRadius || 0};
   object-fit: cover;
+  box-shadow: ${(props) => (props.isShadow ? boxShadow.shadowXS : 'none')};
 `;
-const Avatar = ({ imageURL, ...props }) => {
+
+const Avatar = ({ imageURL, isShadow = false, ...props }) => {
   return (
     <ProfileURL
+      isShadow={isShadow}
       src={imageURL}
       width={props.width}
       height={props.height}
