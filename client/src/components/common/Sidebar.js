f13b7c2d765f38d8bf4311bb759324acc1103a0c
Swan <adeliine.0618@gmail.com>
2022-09-21 12:02:23 +0900

    Feat : Modify style

--- client/src/components/common/Sidebar.js
+++ client/src/components/common/Sidebar.js
@@ -7,19 +7,25 @@ const Wraper = styled.aside`
   display: ${(props) => (props.hasSidebar ? 'flex' : 'none')};
   flex-direction: column;
   margin: ${theme.space.spaceM};
-  max-width: 200px;
+  max-width: 223px;
+  & > * {
+    margin-bottom: ${theme.space.spaceM};
+  }
 `;
+const UserBox = styled.div``;
 
 const Sidebar = ({ hasSidebar }) => {
   return (
     <Wraper hasSidebar={hasSidebar}>
       <Logo />
-      <Avatar
-        imageURL={
-          'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
-        }
-        borderRadius={theme.borderRadius.borderRadiusL}
-      />
+      <UserBox>
+        <Avatar
+          imageURL={
+            'https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png'
+          }
+          borderRadius={theme.borderRadius.borderRadiusL}
+        />
+      </UserBox>
     </Wraper>
   );
 };
