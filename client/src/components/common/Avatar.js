e20bfb58a9374a470ccb461f583ec6426c2c6c88
Swan <adeliine.0618@gmail.com>
2022-09-21 17:44:16 +0900

    Feat : 그림자 삭제

--- client/src/components/common/Avatar.js
+++ client/src/components/common/Avatar.js
@@ -4,8 +4,6 @@ const ProfileURL = styled.img`
   width: ${(props) => props.width || '100%'};
   height: ${(props) => props.height || '100%'};
   border-radius: ${(props) => props.borderRadius || 0};
-  box-shadow: 0px 18px 64px -8px rgba(28, 50, 79, 0.38),
-    0px 4px 24px -3px rgba(28, 55, 90, 0.16);
   object-fit: cover;
 `;
 const Avatar = ({ imageURL, ...props }) => {
