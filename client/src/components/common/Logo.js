3054f13b91c2dd66864ffb32226afdf1e12b8f21
Swan <adeliine.0618@gmail.com>
2022-09-21 17:45:43 +0900

    Feat : 클릭 시 루트페이지로 이동

--- client/src/components/common/Logo.js
+++ client/src/components/common/Logo.js
@@ -1,6 +1,18 @@
 import { ReactComponent as LogoSvg } from '../../assets/logo.svg';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+const LogoButton = styled.div``;
 const Logo = (props) => {
-  return <LogoSvg width={props.width} height={props.height} />;
+  const navigate = useNavigate();
+  return (
+    <LogoButton
+      onClick={() => {
+        navigate('/');
+      }}
+    >
+      <LogoSvg width={props.width} height={props.height} />
+    </LogoButton>
+  );
 };
 
 export default Logo;
