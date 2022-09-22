1ccb1e556a899b870960efd83dc2492f70cc2b46
0SCAR0421 <oscar0421@kakao.com>
2022-09-22 17:58:04 +0900

    Chore : 헤더컴포넌트에 props 전달가능하도록 수정

--- client/src/pages/Layout.js
+++ client/src/pages/Layout.js
@@ -1,6 +1,7 @@
 import Sidebar from '../components/common/Sidebar';
 import Header from '../components/common/Header';
 import styled from 'styled-components';
+import React, { useState } from 'react';
 
 const Container = styled.div`
   margin: 0 auto;
@@ -12,13 +13,14 @@ const Content = styled.section`
   width: 100%;
   max-width: ${(props) => (props.hasCommon ? '977px' : '1200px')};
 `;
-function Layout({ hasCommon = true, ...props }) {
+function Layout({ hasCommon = true, children }) {
+  const [headerData, setHeaderData] = useState({});
   return (
     <Container>
       {hasCommon && <Sidebar />}
       <Content hasCommon={hasCommon}>
-        {hasCommon && <Header />}
-        {props.children}
+        {hasCommon && <Header header={headerData} />}
+        {React.cloneElement(children, { setHeaderData })}
       </Content>
     </Container>
   );
