9bc29d9e201802cdce9491d8dcf9167ca777a820
Swan <adeliine.0618@gmail.com>
2022-09-27 09:30:18 +0900

    Feat : 스타일 삭제

--- client/src/components/TextEditor.js
+++ client/src/components/TextEditor.js
@@ -1,17 +1,6 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.bubble.css';
-import styled from 'styled-components';
-import { space } from '../assets/styles/theme';
-const Container = styled.div`
-  max-width: 710px;
-  margin: auto;
-  margin-top: ${space.spaceM};
-  & > .quill {
-    color: #232629;
-    min-height: 20rem;
-  }
-`;
-const TextEditor = ({ content, setContent }) => {
+const TextEditor = ({ content, onChange }) => {
   const modules = {
     toolbar: [
       [{ color: [] }, { background: [] }],
@@ -27,15 +16,13 @@ const TextEditor = ({ content, setContent }) => {
     ],
   };
   return (
-    <Container>
-      <ReactQuill
-        theme="bubble"
-        value={content}
-        placeholder="당신의 오늘을 공유해 주세요. 글을 드래그하면 멋진 효과들을 만날 수 있답니다!"
-        modules={modules}
-        onChange={setContent}
-      />
-    </Container>
+    <ReactQuill
+      theme="bubble"
+      value={content}
+      placeholder="당신의 오늘을 공유해 주세요. 글을 드래그하면 멋진 효과들을 만날 수 있답니다!"
+      modules={modules}
+      onChange={onChange}
+    />
   );
 };
 
