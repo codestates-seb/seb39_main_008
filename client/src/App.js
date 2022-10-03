9c1ae92f752a64dc9887fb20c19a6719e0ac4010
Swan <adeliine.0618@gmail.com>
2022-10-04 08:42:16 +0900

    Chore : themeprovider app으로 이동

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { path } from './pages/router';
+import { ThemeProvider } from 'styled-components';
+import { light } from './assets/styles/theme';
+import { GlobalStyle } from './assets/styles/GlobalStyle';
 import Loading from './components/common/Loading';
+import { getFromLocalStorage } from './lib/localStorage';
 const Layout = React.lazy(() => import('./pages/Layout'));
 const Root = React.lazy(() => import('./pages/Root'));
 const Error = React.lazy(() => import('./pages/Error'));
@@ -18,124 +22,145 @@ const Userpage = React.lazy(() => import('./pages/Userpage'));
 const People = React.lazy(() => import('./pages/People'));
 const EditDiary = React.lazy(() => import('./pages/EditDiary'));
 const EditBook = React.lazy(() => import('./pages/EditBook'));
-
+const Theme = React.lazy(() => import('./pages/Theme'));
 function App() {
+  const [themeMode, setThemeMode] = useState('light');
+  console.log(themeMode);
+  const theme = light;
+  useEffect(() => {
+    const res = getFromLocalStorage('customTheme');
+    setThemeMode(res);
+    console.log(res);
+  });
+  // const theme = themeMode === 'light' ? light : customTheme;
+  //로컬에서 가져온 값이 있으면 그걸 쓰고, 없으면 디폴트로한다.?
   return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route
-          path="*"
-          element={
-            <Layout>
-              <Error />
-            </Layout>
-          }
-        ></Route>
-
-        <Route
-          path={path.signUp}
-          element={
-            <Layout hasCommon={false}>
-              <Signup />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.logIn}
-          element={
-            <Layout hasCommon={false}>
-              <Login />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.root}
-          element={
-            <Root>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path="*"
+            element={
               <Layout>
-                <Main />
+                <Error />
               </Layout>
-              <Layout hasCommon={false}>
-                <Landing />
-              </Layout>
-            </Root>
-          }
-        ></Route>
-        <Route
-          path={path.bookList}
-          element={
-            <Layout>
-              <BookList />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.book}
-          element={
-            <Layout>
-              <Book />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.makeBook}
-          element={
-            <Layout>
-              <MakeBook />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.editBook}
-          element={
-            <Layout>
-              <EditBook />
-            </Layout>
-          }
-        ></Route>
+            }
+          ></Route>
 
-        <Route
-          path={path.diary}
-          element={
-            <Layout>
-              <Diary />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.writeDiary}
-          element={
-            <Layout>
-              <WriteDiary />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.editDiary}
-          element={
-            <Layout>
-              <EditDiary />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.userpage}
-          element={
-            <Layout>
-              <Userpage />
-            </Layout>
-          }
-        ></Route>
-        <Route
-          path={path.people}
-          element={
-            <Layout>
-              <People />
-            </Layout>
-          }
-        ></Route>
-      </Routes>
-    </Suspense>
+          <Route
+            path={path.signUp}
+            element={
+              <Layout hasCommon={false}>
+                <Signup />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.logIn}
+            element={
+              <Layout hasCommon={false}>
+                <Login />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.root}
+            element={
+              <Root>
+                <Layout>
+                  <Main />
+                </Layout>
+                <Layout hasCommon={false}>
+                  <Landing />
+                </Layout>
+              </Root>
+            }
+          ></Route>
+          <Route
+            path={path.bookList}
+            element={
+              <Layout>
+                <BookList />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.book}
+            element={
+              <Layout>
+                <Book />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.makeBook}
+            element={
+              <Layout>
+                <MakeBook />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.editBook}
+            element={
+              <Layout>
+                <EditBook />
+              </Layout>
+            }
+          ></Route>
+
+          <Route
+            path={path.diary}
+            element={
+              <Layout>
+                <Diary />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.writeDiary}
+            element={
+              <Layout>
+                <WriteDiary />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.editDiary}
+            element={
+              <Layout>
+                <EditDiary />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.userpage}
+            element={
+              <Layout>
+                <Userpage />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.people}
+            element={
+              <Layout>
+                <People />
+              </Layout>
+            }
+          ></Route>
+          <Route
+            path={path.theme}
+            element={
+              <Layout>
+                <Theme />
+              </Layout>
+            }
+          ></Route>
+        </Routes>
+      </Suspense>
+    </ThemeProvider>
   );
 }
 
