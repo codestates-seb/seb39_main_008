87ed2f850e242c7964c699a2bd25c882b3659af4
Swan <adeliine.0618@gmail.com>
2022-10-04 08:59:57 +0900

    Feat : localstorage 래퍼 함수 추가

---
+++ client/src/lib/localStorage.js
@@ -0,0 +1,8 @@
+export const setToLocalStorage = (key, value) => {
+  window.localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getFromLocalStorage = (key) => {
+  const value = window.localStorage.getItem(key);
+  if (value) return JSON.parse(value);
+};
