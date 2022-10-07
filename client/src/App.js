be6a1d959dd9a1d7d32eaedfce8be777852aedbd
Swan <adeliine.0618@gmail.com>
2022-10-07 10:21:29 +0900

    Chore : 프리티어 적용

--- client/src/App.js
+++ client/src/App.js
@@ -88,7 +88,6 @@ function App() {
                   <Main />
                 </Layout>
               ) : (
-
                 <Layout hasCommon={false}>
                   <Landing />
                 </Layout>
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -16,7 +16,6 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect } from 'react';
 import { setToLocalStorage } from '../lib/localStorage';
 
-
 const Container = styled.div`
   text-align: center;
 `;
@@ -106,74 +105,61 @@ const Landing = () => {
   });
 
   return (
-  
-     <Container>
-        <Box1>
-          <Logo width="30%" />
-          <MockupWraper>
-            <AbsoluteImg right={'70%'} bottom={'5%'} width={'35%'} src={img1} />
-            <AbsoluteImg
-              right={'60%'}
-              bottom={'20%'}
-              width={'35%'}
-              src={img2}
-            />
-            <AbsoluteImg right={'8%'} bottom={'40%'} width={'30%'} src={img3} />
-            <AbsoluteImg right={'10%'} bottom={'5%'} width={'18%'} src={img4} />
-            <AbsoluteImg right={'28%'} bottom={'0%'} width={'50%'} src={mac} />
-            <AbsoluteImg right={'25%'} bottom={'0%'} width={'17%'} src={iPad} />
-            <AbsoluteImg
-              right={'20%'}
-              bottom={'0%'}
-              width={'8%'}
-              src={iPhone}
-            />
-          </MockupWraper>
-          <GuideWraper>
-            <GuideBox>
-              <Img src={user1}></Img>
+    <Container>
+      <Box1>
+        <Logo width="30%" />
+        <MockupWraper>
+          <AbsoluteImg right={'70%'} bottom={'5%'} width={'35%'} src={img1} />
+          <AbsoluteImg right={'60%'} bottom={'20%'} width={'35%'} src={img2} />
+          <AbsoluteImg right={'8%'} bottom={'40%'} width={'30%'} src={img3} />
+          <AbsoluteImg right={'10%'} bottom={'5%'} width={'18%'} src={img4} />
+          <AbsoluteImg right={'28%'} bottom={'0%'} width={'50%'} src={mac} />
+          <AbsoluteImg right={'25%'} bottom={'0%'} width={'17%'} src={iPad} />
+          <AbsoluteImg right={'20%'} bottom={'0%'} width={'8%'} src={iPhone} />
+        </MockupWraper>
+        <GuideWraper>
+          <GuideBox>
+            <Img src={user1}></Img>
+            <Text>
+              여러 사람들과 함께 읽고 쓰는 공간.
+              <br /> 같은 고민, 다른 생각을 공유하며 위로와 공감을 나눠요
+            </Text>
+          </GuideBox>
+          <GuideBox>
+            <Text>
+              기록은 남기고 싶은데, <br /> 무슨 내용을 적으면 좋을까요? <br />
+              DUSKHOUR가 추천하는 주제로 적어 보세요
+            </Text>
+            <Img src={user2}></Img>
+          </GuideBox>
+          <GuideBox>
+            <Img src={user3}></Img>
+            <Text>
+              원하는 대로 바꿔보세요 <br /> 나만의 개성이 살아있는 일기장을
+              만들어 보세요
+            </Text>
+          </GuideBox>
+          <GuideBox className="lastBox">
+            <div className="last">
               <Text>
-                여러 사람들과 함께 읽고 쓰는 공간.
-                <br /> 같은 고민, 다른 생각을 공유하며 위로와 공감을 나눠요
+                지금부터 DUSKHOUR와 <br /> 함께 일상을 기록해보세요
               </Text>
-            </GuideBox>
-            <GuideBox>
-              <Text>
-                기록은 남기고 싶은데, <br /> 무슨 내용을 적으면 좋을까요? <br />
-                DUSKHOUR가 추천하는 주제로 적어 보세요
-              </Text>
-              <Img src={user2}></Img>
-            </GuideBox>
-            <GuideBox>
-              <Img src={user3}></Img>
-              <Text>
-                원하는 대로 바꿔보세요 <br /> 나만의 개성이 살아있는 일기장을
-                만들어 보세요
-              </Text>
-            </GuideBox>
-            <GuideBox className="lastBox">
-              <div className="last">
-                <Text>
-                  지금부터 DUSKHOUR와 <br /> 함께 일상을 기록해보세요
-                </Text>
-                <TextButton
-                  fontSize={`var(--fontSizeM)`}
-                  onClick={() => navigate('/signup')}
-                  text={'가입하기'}
-                ></TextButton>
-                <TextButton
-                  fontSize={`var(--fontSizeM)`}
-                  onClick={() => navigate('/main')}
-                  text={'둘러보기'}
-                ></TextButton>
-              </div>
-              <Img src={user4}></Img>
-            </GuideBox>
-          </GuideWraper>
-        </Box1>
-      </Container>
-   
-
+              <TextButton
+                fontSize={`var(--fontSizeM)`}
+                onClick={() => navigate('/signup')}
+                text={'가입하기'}
+              ></TextButton>
+              <TextButton
+                fontSize={`var(--fontSizeM)`}
+                onClick={() => navigate('/main')}
+                text={'둘러보기'}
+              ></TextButton>
+            </div>
+            <Img src={user4}></Img>
+          </GuideBox>
+        </GuideWraper>
+      </Box1>
+    </Container>
   );
 };
 
